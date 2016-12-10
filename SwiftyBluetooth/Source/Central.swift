60c694285bd5b9a0ce4058ffeea90978e06fd74b
tehjord <tehjord@gmail.com>
2016-12-10 15:35:30 -0800

    Switch all callbacks to using a result enum type.

--- SwiftyBluetooth/Source/Central.swift
+++ SwiftyBluetooth/Source/Central.swift
@@ -80,8 +80,8 @@ public enum AsyncCentralState: Int {
 public typealias AsyncCentralStateCallback = (AsyncCentralState) -> Void
 public typealias BluetoothStateCallback = (CBCentralManagerState) -> Void
 public typealias PeripheralScanCallback = (PeripheralScanResult) -> Void
-public typealias ConnectPeripheralCallback = (Error?) -> Void
-public typealias DisconnectPeripheralCallback = (Error?) -> Void
+public typealias ConnectPeripheralCallback = (Result<NoValue>) -> Void
+public typealias DisconnectPeripheralCallback = (Result<NoValue>) -> Void
 
 /// A singleton wrapping a CBCentralManager instance to run CBCentralManager related functions with closures based callbacks instead of the usual CBCentralManagerDelegate interface.
 public final class Central {
@@ -102,15 +102,13 @@ extension Central {
     
     func connect(peripheral: CBPeripheral,
                  timeout: TimeInterval = 10,
-                 completion: @escaping ConnectPeripheralCallback)
-    {
+                 completion: @escaping ConnectPeripheralCallback) {
         centralProxy.connect(peripheral: peripheral, timeout: timeout, completion)
     }
     
     func disconnect(peripheral: CBPeripheral,
                     timeout: TimeInterval = 10,
-                    completion: @escaping DisconnectPeripheralCallback)
-    {
+                    completion: @escaping DisconnectPeripheralCallback) {
         centralProxy.disconnect(peripheral: peripheral, timeout: timeout, completion)
     }
 }
--- SwiftyBluetooth/Source/CentralProxy.swift
+++ SwiftyBluetooth/Source/CentralProxy.swift
@@ -162,8 +162,15 @@ private final class ConnectPeripheralRequest {
     }
     
     func invokeCallbacks(error: Error?) {
+        let result: Result<NoValue> = {
+            if let error = error {
+                return .failure(error)
+            } else {
+                return .success(.noValue)
+            }
+        }()
         for callback in callbacks {
-            callback(error)
+            callback(result)
         }
     }
 }
@@ -172,14 +179,14 @@ extension CentralProxy {
     func connect(peripheral: CBPeripheral, timeout: TimeInterval, _ callback: @escaping ConnectPeripheralCallback) {
         initializeBluetooth { [unowned self] (error) in
             if let error = error {
-                callback(error)
+                callback(.failure(error))
                 return
             }
             
             let uuid = peripheral.identifier
             
             if let cbPeripheral = self.centralManager.retrievePeripherals(withIdentifiers: [uuid]).first , cbPeripheral.state == .connected {
-                callback(nil)
+                callback(.success(.noValue))
                 return
             }
             
@@ -231,8 +238,15 @@ private final class DisconnectPeripheralRequest {
     }
     
     func invokeCallbacks(error: Error?) {
+        let result: Result<NoValue> = {
+            if let error = error {
+                return .failure(error)
+            } else {
+                return .success(.noValue)
+            }
+        }()
         for callback in callbacks {
-            callback(error)
+            callback(result)
         }
     }
 }
@@ -242,7 +256,7 @@ extension CentralProxy {
         initializeBluetooth { [unowned self] (error) in
             
             if let error = error {
-                callback(error)
+                callback(.failure(error))
                 return
             }
             
@@ -250,7 +264,7 @@ extension CentralProxy {
             
             if let cbPeripheral = self.centralManager.retrievePeripherals(withIdentifiers: [uuid]).first,
                 (cbPeripheral.state == .disconnected || cbPeripheral.state == .disconnecting) {
-                callback(nil)
+                callback(.success(.noValue))
                 return
             }
             
