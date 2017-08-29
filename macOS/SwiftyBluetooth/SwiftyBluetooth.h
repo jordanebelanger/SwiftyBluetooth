b50683bb3f4107fed90b1386db220c6870ca30b2
Petro Ladkin <petro.ladkin@gmail.com>
2017-08-29 11:22:04 +0300

    Added macOS support

---
+++ macOS/SwiftyBluetooth/SwiftyBluetooth.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyBluetooth.h
+//  SwiftyBluetooth
+//
+//  Created by Petro Ladkin on 8/29/17.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftyBluetooth.
+FOUNDATION_EXPORT double SwiftyBluetoothVersionNumber;
+
+//! Project version string for SwiftyBluetooth.
+FOUNDATION_EXPORT const unsigned char SwiftyBluetoothVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyBluetooth/PublicHeader.h>
+
+
